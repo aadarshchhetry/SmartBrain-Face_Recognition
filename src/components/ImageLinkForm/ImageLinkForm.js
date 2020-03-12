f32ed0ae55e26dc8c5ec50b7322442765334061d
Aadarsh chhetry <41640633+aadarshchhetry@users.noreply.github.com>
2020-03-12 16:12:27 +0530

    added bootstrap

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -4,6 +4,7 @@ const ImageLinkForm = () => {
     return (
         <nav>
             <p>Please paste the image link here</p>
+            <input class="form-control" type="text" placeholder="Default input"></input>
         </nav>
     );
 }
