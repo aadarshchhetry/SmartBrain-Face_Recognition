02933b2c393efadc8f299efaa14c935fe68b3ae9
Aadarsh chhetry <41640633+aadarshchhetry@users.noreply.github.com>
2020-03-12 17:44:52 +0530

    created rank component

--- src/App.js
+++ src/App.js
@@ -2,17 +2,15 @@ import React from 'react';
 import Logo from './components/Logo/Logo'
 import Navigation from './components/Navigation/Navigation';
 import "./App.css";
+import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      {
-        /*<Imagelink_formation />
-          <Facerecognition /> */
-      }
       <Logo />
+      <Rank />
       <ImageLinkForm />
     </div>
   );
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Rank = () => {
+    return (
+        <nav>
+            <p><h3>Your rank is..</h3></p>
+            <h2>#2</h2>
+        </nav>
+    );
+}
+
+export default Rank;
