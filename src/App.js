f489cf396c31de8006c6ee96e33345b638ea674a
Aadarsh chhetry <41640633+aadarshchhetry@users.noreply.github.com>
2020-03-13 11:53:36 +0530

    fixes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Logo from './components/Logo/Logo'
 import Navigation from './components/Navigation/Navigation';
 import "./App.css";
 import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Particles from 'react-particles-js';
-import ImageFile from '/components/ImageFile/ImageFile'
+import ImageFile from './components/ImageFile/ImageFile';
 
-function App() {
-  return (
-    <div className="App">
-      <Particles className="particles"/>
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      <ImageFile />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = ''
+  }
+  onInputChange = (event) => {
+    console.log(event.target.value)
+  }
+  OnSubmitButton = () =>{
+    console.log('click')
+  }
+  render(){
+    return (
+      <div className= "App">
+        <Particles className="particles"/>
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm 
+          OnInputChange= {this.OnInputChange}
+          OnSubmitButton= {this.OnSubmitButton} 
+        />
+        <ImageFile />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({OnInputchange, OnSubmitbutton}) => {
     return (
         <nav>
             
             <div class="col-xl-5 col-lg-6 col-md-8 col-sm-10 mx-auto text-center form p-4">
                 <form class="form justify-content-center" >
                     <label><h3>Please paste image link here</h3></label>
-                    <input type="text" class="form-control mb-2 mr-sm-2" id="imagelinkform" placeholder="paste link here"></input>
-                    <button type="submit" class="btn btn-primary mb-2">Detect</button>
+                    <input type="text" class="form-control mb-2 mr-sm-2" id="imagelinkform" placeholder="paste link here" onChange={OnInputchange}></input>
+                    <button type="submit" class="btn btn-primary mb-2" onClick={OnSubmitbutton}>Detect</button>
                 </form>
             </div>
         </nav>
